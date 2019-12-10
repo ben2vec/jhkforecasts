67f44980467acf45680fbf1baee314e55b6bb661
jhkersting <56937882+jhkersting@users.noreply.github.com>
2019-12-10 16:48:47 -0600

    Your Message

---
+++ test.js
@@ -0,0 +1,10 @@
+<script type='text/javascript' src='https://online.tableau.com/javascripts/api/viz_v1.js'></script> 
+<div class='tableauPlaceholder' style='width:800; height:600;'> 
+<object class='tableauViz' width='800' height='600' style='display:none;'>
+   <param name='host_url' value='https%3A%2F%2Fonline.tableau.com%2F' /> 
+   <param name='site_root' value='&#47;t&#47;Sales' /> 
+   <param name='name' value='MyCoSales&#47;SalesScoreCard&#47;' /> 
+   <param name='tabs' value='yes' /> 
+   <param name='toolbar' value='yes' />
+</object>
+</div> 
