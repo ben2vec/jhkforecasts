1db113fd851edb90116a9769b507848de3f73d04
jhkersting <56937882+jhkersting@users.noreply.github.com>
2020-01-26 22:37:43 -0600

    Your Message

--- democratic_primary/topline copy.js
+++ democratic_primary/topline copy.js
@@ -7,13 +7,13 @@ var delscale = d3.scaleLinear()
 
 
 
-d3.csv("topline.csv", function (error, data) {
+d3.csv("candsoverview.csv", function (error, data) {
 
   
+  
 
-
-  var svg = d3.select("#topline").append("svg")
-    .attr("viewBox", "0 0 1000 430")
+  var svg = d3.select("#overview").append("svg")
+    .attr("viewBox", "0 0 1000 400")
     .append('g')
 
   
@@ -24,7 +24,7 @@ svg.append("rect")
 
   var svgrepeat = svg.append('g')
     .attr('class', 'grepeat')
-    .attr("transform", "translate(" + 0 + "," + 90 + ")")
+    .attr("transform", "translate(" + 0 + "," + 150 + ")")
 
 
 
@@ -32,49 +32,53 @@ svg.append("rect")
     .data(data)
     .enter().append('g')
     .attr("class", "repeat")
-    .attr("transform", function (d, i) { return "translate(0," + i * 40 + ")" })
+    .attr("transform", function (d, i) { return "translate(" + i * 100 + ",0)" })
 
+    repeat.append("a").attr("xlink:href",d=>d.candidate).append("image")
+    .attr("xlink:href",  d=>d.candidate+".jpg")
+    .attr("x", 120)
+    .attr("y", -130)
+    .attr("height", 100)
+    .attr("width", 60)
+    .attr("anchor","middle").on('mouseover', function(d) {
+                                  
+      d3.select(this)
+      .attr("x", 100)
+      .attr("width",100)
+      
 
+        
+      })
+      .on('mouseout', 
+      function(d) {
+      
+        d3.select(this)
+        .attr("x", 120)
+        .attr("width",60)
+        
+
+      });;
+    
     repeat.append("rect")
-    .attr("x",680)
+    .attr("x",110)
     .attr("y",-25)
-    .attr("width",100)
-    .attr("height",40)
+    .attr("rx",12)
+    .attr("ry",12)
+    .attr("width",80)
+    .attr("height",60)
     .attr("fill",d=> winscale(d.win))
 
-    repeat.append("rect")
-    .attr("x",780)
-    .attr("y",-25)
-    .attr("width",100)
-    .attr("height",40)
-    .attr("fill",d=> delscale(d.delegates))
-
-    repeat.append("rect")
-    .attr("x",880)
-    .attr("y",-25)
-    .attr("width",100)
-    .attr("height",40)
-    .attr("fill",d=> winscale(d.voteShare))
-
-  repeat.append("text")
-    .attr("class", "repeat-text")
-    .attr("x", 20)
-    .attr("y", 0)
-    .style("fill", "Black")
-    .style("font-size", 20)
-    .attr("font-weight", 500)
-    .text(d => d.category)
-    .attr("text-anchor", "start")
+    
 
     repeat.append("text")
     .attr("class", "repeat-text")
-    .attr("x", 775)
-    .attr("y", 0)
-    .style("fill", "Black")
-    .style("font-size", 20)
-    .attr("font-weight", 500)
+    .attr("x", 150)
+    .attr("y", 10)
+    .style("fill", d => d.win > 50? "white":"black")
+    .style("font-size",d=> Math.sqrt(d.win)+15)
+    .attr("font-weight", 700)
     .text(d => d.win+"%")
-    .attr("text-anchor", "end")
+    .attr("text-anchor", "middle")
 
     repeat.append("text")
     .attr("class", "repeat-text")
@@ -94,48 +98,17 @@ svg.append("rect")
     .style("fill", "Black")
     .style("font-size", 20)
     .attr("font-weight", 500)
-    .text(d => d.voteShare+"%")
+    .text(d => d.vote+"%")
     .attr("text-anchor", "end")
     
-    repeat.append("line")
-    .attr("x1",10)
-    .attr("x2",990)
-    .attr("y1",-25)
-    .attr("y2",-25)
-    .attr("stroke","#e1e5e8")
-    .attr("stroke-width",1)
+    
+
+   
+
 
     svg.append("text")
-    .attr("x",20)
-    .attr("y",55)
-    .style("fill", "Black")
-    .style("font-size", 25)
-    .attr("font-weight",700)
-    .text("Candidate")
-
-    svg.append("text")
-    .attr("x",830)
-    .attr("y",55)
-    .style("fill", "Black")
-    .style("font-size", 20)
-    .attr("font-weight",700)
-    .attr("text-anchor","end")
-    .text("Delegates")
-    .attr("text-anchor","middle")
-
-    svg.append("text")
-    .attr("x",930)
-    .attr("y",55)
-    .style("fill", "Black")
-    .style("font-size", 20)
-    .attr("font-weight",700)
-    .attr("text-anchor","end")
-    .text("Vote Share")
-    .attr("text-anchor","middle")
-
-    svg.append("text")
-    .attr("x",730)
-    .attr("y",30)
+    .attr("x",50)
+    .attr("y",140)
     .style("fill", "Black")
     .style("font-size", 20)
     .attr("font-weight",700)
@@ -144,19 +117,13 @@ svg.append("rect")
     .text("Win")
     
     svg.append("text")
-    .attr("x",730)
-    .attr("y",55)
+    .attr("x",50)
+    .attr("y",160)
     .style("fill", "Black")
     .style("font-size", 20)
     .attr("font-weight",700)
     .attr("text-anchor","middle")
     .text("Nomination")
 
-    svg.append("line")
-    .attr("x1",10)
-    .attr("x2",980)
-    .attr("y1",65)
-    .attr("y2",65)
-    .attr("stroke","black")
-    .attr("stroke-width",3)
+    
 })
