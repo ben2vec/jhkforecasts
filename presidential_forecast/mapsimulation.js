8e909bf5743b4366ee66f611e143f7b6e7a09509
jhkersting <56937882+jhkersting@users.noreply.github.com>
2020-01-30 01:42:29 -0600

    s

--- presidential_forecast/mapsimulation.js
+++ presidential_forecast/mapsimulation.js
@@ -1,38 +1,28 @@
 
-/*  This visualization was made possible by modifying code provided by:
- 
-Scott Murray, Choropleth example from "Interactive Data Visualization for the Web" 
-https://github.com/alignedleft/d3-book/blob/master/chapter_12/05_choropleth.html   
-    
-Malcolm Maclean, tooltips example tutorial
-http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html
- 
-Mike Bostock, Pie Chart Legend
-http://bl.ocks.org/mbostock/3888852  */
 
 
-//Width and height of map
+
 var width3 = 1020;
 var height3 = 500;
 
-// D3 Projection
+
 var projection = d3.geoAlbersUsa()
-  .translate([width3 / 2, height3 / 2])    // translate to center of screen
-  .scale([900]);          // scale things down so see entire US
-
-// Define path generator
-var path = d3.geoPath()               // path generator that will convert GeoJSON to SVG paths
-  .projection(projection);  // tell path generator to use albersUsa projection
+  .translate([width3 / 2, height3 / 2])   
+  .scale([900]);         
 
 
-// Define linear scale for output
+var path = d3.geoPath()             
+  .projection(projection);  
+
+
+
 var color = d3.scaleLinear()
   .domain([-30, 0, 30])
   .range(["#0091FF", "white", "#FF6060"]);
 
 var gopwincol = "#FF6060"
 var demwincol = "#0091FF"
-//Create SVG element and append map to the SVG
+
 var svg = d3.select("#usmap")
   .append("svg")
   .attr("viewBox", '100 -50 820 950');
@@ -43,7 +33,7 @@ simulation = Math.random()
 
 norminv = jStat.normal.inv(.9, 0, 1)
 console.log(formatValue(norminv))
-// Load in my states data!
+
 d3.csv("simulation.csv", function (data) {
 
   var data = data.map((d, i) => {
@@ -68,42 +58,56 @@ d3.csv("simulation.csv", function (data) {
     d.thirdvote = d.thirdvoteraw / ((d.totalraw / 100));
     d.margin = d.gopvote - d.demvote;
     d.gopev = d.margin >= 0 ? d.electoralvotes : 0;
+    d.absmargin = Math.abs(d.margin);
+    return d;
+  })
+  
+  var data = data.sort((a, b) => a.margin-b.margin)
+  
+  
+
+  data.forEach(function (d,i) {
+    d.index = i+1;
+    d.indexev = d.index==1?0:data[i-1].indexev+data[i-1].electoralvotes;
     return d;
   })
 
-
+  
+  console.log(data)
+  
   var gopev = d3.sum(data, d => d.gopev)
   var demev = 538 - gopev
-    console.log(gopev)
+    
+  console.log(gopev)
   console.log(data)
   console.log(data[0].gopvote)
   console.log(data[0].demvote)
   console.log(data[0].thirdvote)
   console.log(data[0].margin)
-  // Load GeoJSON data and merge with states data
+ 
   d3.json("us-states.json", function (json) {
 
-    // Loop through each state data value in the .csv file
+    
     for (var i = 0; i < data.length; i++) {
 
-      // Grab State Name
+      
       var dataState = data[i].state;
 
-      // Grab data value 
+       
       var margin = data[i].margin;
-      // Find the corresponding state inside the GeoJSON
+      
       for (var j = 0; j < json.features.length; j++) {
         var jsonState = json.features[j].properties.name;
 
         if (dataState == jsonState) {
 
-          // Copy the data value into the JSON
+          
           json.features[j].properties.margin = margin
 
             ;
 
 
-          // Stop looking through the JSON
+         
 
           break;
         }
@@ -111,7 +115,7 @@ d3.csv("simulation.csv", function (data) {
     }
     console.log(json.features)
     console.log(data)
-    // Bind the data to the SVG and create one path per GeoJSON feature
+    
     svg.append("g")
       .attr("id", "margin")
       .selectAll("path2")
@@ -136,6 +140,17 @@ d3.csv("simulation.csv", function (data) {
       .style("fill", d=> d.properties.margin>0? gopwincol:demwincol)
       .attr("text-anchor", "middle")
 
+
+      var x = d3.scaleLinear()
+        .range([0,538])
+        .domain([120,880])
+
+      svg.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("y",900)
+      .attr("x",)
       
 
 
