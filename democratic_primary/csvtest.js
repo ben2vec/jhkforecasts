49eb6635cfac9a345e4945674e8b6697194c9928
jhkersting <56937882+jhkersting@users.noreply.github.com>
2020-02-01 03:22:12 -0600

    s

---
+++ democratic_primary/csvtest.js
@@ -0,0 +1,20 @@
+const d3 = require('d3');
+const ObjectsToCsv = require('objects-to-csv')
+
+// Sample data - two columns, three rows:
+const data = [
+  {code: 'CA', name: 'California'},
+  {code: 'TX', name: 'Texas'},
+  {code: 'NY', name: 'New York'},
+];
+
+// If you use "await", code must be inside an asynchronous function:
+(async () => {
+  const csv = new ObjectsToCsv(data);
+
+  // Save to file:
+  await csv.toDisk('./jackstest.csv');
+
+  // Return the CSV file as string:
+  console.log(await csv.toString());
+})();
