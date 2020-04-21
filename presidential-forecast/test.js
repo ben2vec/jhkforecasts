39bfc7f3004a1d0e6ab7d81fd3386d64d3570319
jhkersting <56937882+jhkersting@users.noreply.github.com>
2020-04-21 12:43:35 -0500

    s

--- presidential-forecast/test.js
+++ presidential-forecast/test.js
@@ -6,7 +6,7 @@ var map_states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Col
 var map_labels = [{ "state": "Alabama", "label": "AL", "xValue": 637, "yValue": 338.6934 }, { "state": "Alaska", "label": "AK", "xValue": 245, "yValue": 400 }, { "state": "Arizona", "label": "AZ", "xValue": 315, "yValue": 306.5801 }, { "state": "Arkansas", "label": "AR", "xValue": 560, "yValue": 315.6387 }, { "state": "California", "label": "CA", "xValue": 223, "yValue": 253.9219 }, { "state": "Colorado", "label": "CO", "xValue": 400, "yValue": 245.5645 }, { "state": "Connecticut", "label": "CT", "xValue": -1000, "yValue": -1000 }, { "state": "Delaware", "label": "DE", "xValue": -1000, "yValue": -1000 }, { "state": "District of Columbia", "label": "DC", "xValue": -1000, "yValue": -1000 }, { "state": "Florida", "label": "FL", "xValue": 714, "yValue": 397.8154 }, { "state": "Georgia", "label": "GA", "xValue": 680.0117, "yValue": 335.2354 }, { "state": "Hawaii", "label": "HI", "xValue": 380, "yValue": 465 }, { "state": "Idaho", "label": "ID", "xValue": 310.1851, "yValue": 155 }, { "state": "Illinois", "label": "IL", "xValue": 596.6602, "yValue": 231.2954 }, { "state": "Indiana", "label": "IN", "xValue": 633.4111, "yValue": 228.4214 }, { "state": "Iowa", "label": "IA", "xValue": 545.8457, "yValue": 202.6782 }, { "state": "Kansas", "label": "KS", "xValue": 487, "yValue": 259.1592 }, { "state": "Kentucky", "label": "KY", "xValue": 655.1484, "yValue": 264.9658 }, { "state": "Louisiana", "label": "LA", "xValue": 561.4404, "yValue": 369.8135 }, { "state": "Maine", "label": "ME", "xValue": 807.3105, "yValue": 109.855 }, { "state": "Maryland", "label": "MD", "xValue": -1000, "yValue": -1000 }, { "state": "Massachusetts", "label": "MA", "xValue": -1000, "yValue": -1000 }, { "state": "Michigan", "label": "MI", "xValue": 645.6465, "yValue": 181.3647 }, { "state": "Minnesota", "label": "MN", "xValue": 530.8594, "yValue": 141.5874 }, { "state": "Mississippi", "label": "MS", "xValue": 598.6016, "yValue": 342.1514 }, { "state": "Missouri", "label": "MO", "xValue": 557, "yValue": 261.123 }, { "state": "Montana", "label": "MT", "xValue": 370.0981, "yValue": 112.7705 }, { "state": "Nebraska", "label": "NE", "xValue": 473.8364, "yValue": 210.0527 }, { "state": "Nevada", "label": "NV", "xValue": 267.8765, "yValue": 219.0957 }, { "state": "New Hampshire", "label": "NH", "xValue": -1000, "yValue": -1000 }, { "state": "New Jersey", "label": "NJ", "xValue": 785, "yValue": 210 }, { "state": "New Mexico", "label": "NM", "xValue": 385.3774, "yValue": 314.1035 }, { "state": "New York", "label": "NY", "xValue": 753.5781, "yValue": 163.2588 }, { "state": "North Carolina", "label": "NC", "xValue": 728.6084, "yValue": 284.5029 }, { "state": "North Dakota", "label": "ND", "xValue": 467.0742, "yValue": 117.3823 }, { "state": "Ohio", "label": "OH", "xValue": 670.7197, "yValue": 219.4883 }, { "state": "Oklahoma", "label": "OK", "xValue": 500.1963, "yValue": 306.418 }, { "state": "Oregon", "label": "OR", "xValue": 240.2783, "yValue": 139.5654 }, { "state": "Pennsylvania", "label": "PA", "xValue": 730.3535, "yValue": 200.856 }, { "state": "Rhode Island", "label": "RI", "xValue": -1000, "yValue": -1000 }, { "state": "South Carolina", "label": "SC", "xValue": 712.4395, "yValue": 315.6387 }, { "state": "South Dakota", "label": "SD", "xValue": 468.0742, "yValue": 163.5166 }, { "state": "Tennessee", "label": "TN", "xValue": 640.8594, "yValue": 290.8193 }, { "state": "Texas", "label": "TX", "xValue": 480.9902, "yValue": 374.2861 }, { "state": "Utah", "label": "UT", "xValue": 330.1084, "yValue": 234.978 }, { "state": "Vermont", "label": "VT", "xValue": -1000, "yValue": -1000 }, { "state": "Virginia", "label": "VA", "xValue": 731.0264, "yValue": 252.7842 }, { "state": "Washington", "label": "WA", "xValue": 256.9365, "yValue": 88.0762 }, { "state": "West Virginia", "label": "WV", "xValue": 701, "yValue": 243 }, { "state": "Wisconsin", "label": "WI", "xValue": 585.2529, "yValue": 163.2588 }, { "state": "Wyoming", "label": "WY", "xValue": 385.9287, "yValue": 179.6255 }]
 var projection = d3.geoAlbersUsa()
     .translate([widthmap / 2, heightmap / 2])
-    .scale([600]);
+    .scale([900]);
 
 var path = d3.geoPath()
     .projection(projection);
@@ -16,26 +16,46 @@ var color = d3.scaleLinear()
     .domain([0, 50, 100])
     .range(["#0091FF", "white", "#FF6060"]);
 
-var map = d3.select("#usmap")
+var svg = d3.select("#usmap")
     .append("svg")
     .attr("viewBox", '75 -50 900 550');
 
 d3.csv("https://data.jhkforecasts.com/2020-presidential.csv", data => {
+    var data = data.map((d, i) => {
+        return {
+            state: d.state,
+            party: d.party,
+            win: +d.win,
+        }
+    })
+    var data = data.splice(data.length - 171, data.length)
+    var data = data.filter(d=>d.party == "gop")
+    var states = data.map(d=>{
+        return d.state
+    })
+    var win_pct = data.map(d=>{
+        return d.win
+    })
+    
 
+    console.log(data)
+    d3.json("us.json", function (us) {
+        for(let j=0;j<us.objects.states.geometries.length;j++){
+            var state = us.objects.states.geometries[j].properties.name
+            var win = win_pct[states.indexOf(state)]
+            us.objects.states.geometries[j].properties.win = win
+        }
+        console.log(topojson.feature(us, us.objects.states).features)
 
-    d3.json("House_20.json", function (house) {
-        console.log(house.features)
-
-        map.selectAll("path")
-            .data(house.features)
+        svg.selectAll("path")
+            .data(topojson.feature(us, us.objects.states).features)
             .enter()
             .append("path")
-            .attr("class", "states")
             .attr("d", path)
-            .style("stroke", "black")
-            .style("stroke-width", ".3")
-            .style("fill", "white")
-        
+            .attr("fill",d=>color(d.properties.win))
+            .attr("stroke", "black")
+            .attr("stroke-width", .5);
+
     })
 
 
