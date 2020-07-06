afc10fd5180ce9d0e80d96b574692a45c7a7eba2
jhkersting <56937882+jhkersting@users.noreply.github.com>
2020-07-06 18:28:51 -0500

    s

---
+++ 2020-elections.js
@@ -0,0 +1,274 @@
+var presInfo = [{ 'state': 'Alabama', 'label': 'AL', 'radius': 16.43, 'x': 413, 'y': 332 }, { 'state': 'Alaska', 'label': 'AK', 'radius': 9.49, 'x': 41, 'y': 19 }, { 'state': 'Arizona', 'label': 'AZ', 'radius': 18.17, 'x': 172, 'y': 282 }, { 'state': 'Arkansas', 'label': 'AR', 'radius': 13.42, 'x': 325, 'y': 290 }, { 'state': 'California', 'label': 'CA', 'radius': 40.62, 'x': 103, 'y': 237 }, { 'state': 'Colorado', 'label': 'CO', 'radius': 16.43, 'x': 224, 'y': 249 }, { 'state': 'Connecticut', 'label': 'CT', 'radius': 14.49, 'x': 586, 'y': 128 }, { 'state': 'Delaware', 'label': 'DE', 'radius': 9.49, 'x': 557, 'y': 183 }, { 'state': 'District of Columbia', 'label': 'DC', 'radius': 9.49, 'x': 536, 'y': 193 }, { 'state': 'Florida', 'label': 'FL', 'radius': 29.5, 'x': 483, 'y': 380 }, { 'state': 'Georgia', 'label': 'GA', 'radius': 21.91, 'x': 443, 'y': 298 }, { 'state': 'Hawaii', 'label': 'HI', 'radius': 10.95, 'x': 88, 'y': 372 }, { 'state': 'Idaho', 'label': 'ID', 'radius': 10.95, 'x': 188, 'y': 173 }, { 'state': 'Illinois', 'label': 'IL', 'radius': 24.49, 'x': 359, 'y': 207 }, { 'state': 'Indiana', 'label': 'IN', 'radius': 18.17, 'x': 413, 'y': 207 }, { 'state': 'Iowa', 'label': 'IA', 'radius': 13.42, 'x': 306, 'y': 195 }, { 'state': 'Kansas', 'label': 'KS', 'radius': 13.42, 'x': 266, 'y': 248 }, { 'state': 'Kentucky', 'label': 'KY', 'radius': 15.49, 'x': 411, 'y': 251 }, { 'state': 'Louisiana', 'label': 'LA', 'radius': 15.49, 'x': 326, 'y': 335 }, { 'state': 'Maine', 'label': 'ME', 'radius': 7.75, 'x': 628, 'y': 26 }, { 'state': 'Maryland', 'label': 'MD', 'radius': 17.32, 'x': 505, 'y': 185 }, { 'state': 'Massachusetts', 'label': 'MA', 'radius': 18.17, 'x': 607, 'y': 89 }, { 'state': 'Michigan', 'label': 'MI', 'radius': 21.91, 'x': 418, 'y': 149 }, { 'state': 'Minnesota', 'label': 'MN', 'radius': 17.32, 'x': 304, 'y': 142 }, { 'state': 'Mississippi', 'label': 'MS', 'radius': 13.42, 'x': 373, 'y': 324 }, { 'state': 'Missouri', 'label': 'MO', 'radius': 17.32, 'x': 329, 'y': 251 }, { 'state': 'Montana', 'label': 'MT', 'radius': 9.49, 'x': 206, 'y': 131 }, { 'state': 'Nebraska', 'label': 'NE', 'radius': 7.75, 'x': 258, 'y': 209 }, { 'state': 'Nevada', 'label': 'NV', 'radius': 13.42, 'x': 167, 'y': 220 }, { 'state': 'New Hampshire', 'label': 'NH', 'radius': 10.95, 'x': 612, 'y': 54 }, { 'state': 'New Jersey', 'label': 'NJ', 'radius': 20.49, 'x': 551, 'y': 147 }, { 'state': 'New Mexico', 'label': 'NM', 'radius': 12.25, 'x': 215, 'y': 303 }, { 'state': 'New York', 'label': 'NY', 'radius': 29.5, 'x': 548, 'y': 81 }, { 'state': 'North Carolina', 'label': 'NC', 'radius': 21.21, 'x': 499, 'y': 278 }, { 'state': 'North Dakota', 'label': 'ND', 'radius': 9.49, 'x': 257, 'y': 136 }, { 'state': 'Ohio', 'label': 'OH', 'radius': 23.24, 'x': 459, 'y': 191 }, { 'state': 'Oklahoma', 'label': 'OK', 'radius': 14.49, 'x': 270, 'y': 294 }, { 'state': 'Oregon', 'label': 'OR', 'radius': 14.49, 'x': 124, 'y': 176 }, { 'state': 'Pennsylvania', 'label': 'PA', 'radius': 24.49, 'x': 498, 'y': 132 }, { 'state': 'Rhode Island', 'label': 'RI', 'radius': 10.95, 'x': 619, 'y': 126 }, { 'state': 'South Carolina', 'label': 'SC', 'radius': 16.43, 'x': 487, 'y': 322 }, { 'state': 'South Dakota', 'label': 'SD', 'radius': 9.49, 'x': 257, 'y': 167 }, { 'state': 'Tennessee', 'label': 'TN', 'radius': 18.17, 'x': 379, 'y': 284 }, { 'state': 'Texas', 'label': 'TX', 'radius': 33.76, 'x': 271, 'y': 355 }, { 'state': 'Utah', 'label': 'UT', 'radius': 13.42, 'x': 204, 'y': 218 }, { 'state': 'Vermont', 'label': 'VT', 'radius': 9.49, 'x': 585, 'y': 47 }, { 'state': 'Virginia', 'label': 'VA', 'radius': 19.75, 'x': 508, 'y': 229 }, { 'state': 'Washington', 'label': 'WA', 'radius': 18.97, 'x': 154, 'y': 131 }, { 'state': 'West Virginia', 'label': 'WV', 'radius': 12.25, 'x': 451, 'y': 242 }, { 'state': 'Wisconsin', 'label': 'WI', 'radius': 17.32, 'x': 359, 'y': 146 }, { 'state': 'Wyoming', 'label': 'WY', 'radius': 9.49, 'x': 214, 'y': 177 }, { 'state': 'Maine-1', 'label': '1', 'radius': 5.48, 'x': 612, 'y': 26 }, { 'state': 'Maine-2', 'label': '2', 'radius': 5.48, 'x': 644, 'y': 26 }, { 'state': 'Nebraska-1', 'label': '1', 'radius': 5.48, 'x': 242, 'y': 209 }, { 'state': 'Nebraska-2', 'label': '2', 'radius': 5.48, 'x': 258, 'y': 193 }, { 'state': 'Nebraska-3', 'label': '3', 'radius': 5.48, 'x': 274, 'y': 209 }]
+var colors = ['#FF6060', '#0091FF', '#FFE130']
+
+var category = ['gop', 'dem', 'third']
+
+var cand_colors = d3.scaleOrdinal()
+    .domain(category)
+    .range(['#FF6060', '#0091FF', '#FFE130'])
+
+var tformat = d3.timeFormat('%m/%d/%Y')
+var dp = d3.timeParse('%m/%d/%y')
+var timeparse = d3.timeParse('%m/%d/%y %H:%M')
+var nf = d3.format('.1f')
+var updated_format = d3.timeFormat('%b. %d %Y %I:%M %p')
+var color = d3.scaleLinear()
+    .domain([0, 50, 100])
+    .range(['#0091FF', 'white', '#FF6060']);
+
+queue()
+    .defer(d3.csv, 'https://data.jhkforecasts.com/2020-presidential.csv')
+    .defer(d3.csv, 'https://data.jhkforecasts.com/2020-senate.csv')
+    .defer(d3.csv, 'https://data.jhkforecasts.com/2020-house.csv')
+    .defer(d3.json, 'https://projects.jhkforecasts.com/presidential-forecast/us-states.json')
+    .await(ready)
+
+function ready(error, pres, senate, house, json) {
+    if (error) throw error
+    var pres = pres.filter(d => d.party == 'gop')
+    var prestoday = pres.slice(pres.length - 57, pres.length)
+    var trumpWin = +pres[pres.length - 1].win
+
+    presInfo.forEach((d, i) => {
+        var state = d.state
+        d.win = +prestoday.filter(d => d.state == state)[0].win
+    })
+    console.log(presInfo)
+
+    var bubblemap = d3.select('#bubblemap')
+        .append('svg')
+        .attr('viewBox', '-150 0 1000 450')
+
+    bubblemap
+        .append('text')
+        .text('Donald TRUMP')
+        .attr('x', '830')
+        .attr('y', 20)
+        .attr('fill', d => 'black')
+        .attr('text-anchor', 'end')
+        .attr('dominant-baseline', 'central')
+        .attr('font-size', 20)
+        .style('font-weight', '100')
+        .style('font-family', 'sf-mono')
+
+    bubblemap
+        .append('text')
+        .text(nf(trumpWin) + '%')
+        .attr('x', '830')
+        .attr('y', 50)
+        .attr('fill', colors[0])
+        .attr('text-anchor', 'end')
+        .attr('dominant-baseline', 'central')
+        .attr('font-size', 25)
+        .style('font-weight', '100')
+        .style('font-family', 'sf-mono')
+
+
+    bubblemap
+        .append('text')
+        .text(nf(100 - trumpWin) + '%')
+        .attr('x', '-130')
+        .attr('y', 50)
+        .attr('fill', colors[1])
+        .attr('text-anchor', 'start')
+        .attr('dominant-baseline', 'central')
+        .attr('font-size', 25)
+        .style('font-weight', '100')
+        .style('font-family', 'sf-mono')
+
+    bubblemap
+        .append('text')
+        .text('Joseph Biden')
+        .attr('x', '-130')
+        .attr('y', 20)
+        .attr('fill', d => 'black')
+        .attr('text-anchor', 'start')
+        .attr('dominant-baseline', 'central')
+        .attr('font-size', 20)
+        .style('font-weight', '100')
+        .style('font-family', 'sf-mono')
+
+    bubblemap.selectAll('circ')
+        .data(presInfo)
+        .enter()
+        .append('circle')
+        .attr('cx', d => d.x)
+        .attr('cy', d => d.y)
+        .attr('r', d => d.radius)
+        .attr('fill', d => color(d.win))
+
+    bubblemap.selectAll('labels')
+        .data(presInfo)
+        .enter()
+        .append('text')
+        .text(d => d.label)
+        .attr('x', d => d.x)
+        .attr('y', d => d.y)
+        .attr('fill', d => Math.abs(50 - d.win) > 15 ? 'white' : 'black')
+        .attr('text-anchor', 'middle')
+        .attr('dominant-baseline', 'central')
+        .attr('font-size', 8)
+        .style('font-weight', '500')
+        .style('font-family', 'sf-mono')
+
+    bubblemap.selectAll('circ')
+        .data(presInfo)
+        .enter()
+        .append('a')
+        .attr('href', '#statespres')
+        .append('circle')
+        .attr('class', 'statesover')
+        .attr('cx', d => d.x)
+        .attr('cy', d => d.y)
+        .attr('r', d => d.radius)
+        .on('click', function (d) {
+            presState(d.state)
+        })
+
+    function presState(state) {
+
+        d3.select('.presChange').remove()
+        var stateData = pres.filter(d => d.state == state)
+        var svg = d3.select('#statespres')
+            .append('svg')
+            .attr('class', 'presChange')
+            .attr('viewBox', '0 0 1000 200')
+        console.log(stateData)
+
+        svg.append('text')
+            .text(state)
+            .attr('x', '500')
+            .attr('y', 30)
+            .attr('fill', d => 'black')
+            .attr('text-anchor', 'middle')
+            .attr('dominant-baseline', 'central')
+            .attr('font-size', 30)
+            .style('font-weight', '100')
+            .style('font-family', 'sf-mono')
+
+        svg
+            .append('text')
+            .text('Donald TRUMP')
+            .attr('x', '980')
+            .attr('y', 30)
+            .attr('fill', d => 'black')
+            .attr('text-anchor', 'end')
+            .attr('dominant-baseline', 'central')
+            .attr('font-size', 20)
+            .style('font-weight', '100')
+            .style('font-family', 'sf-mono')
+
+        svg
+            .append('text')
+            .text(nf(stateData[stateData.length - 1].win) + '%')
+            .attr('x', '980')
+            .attr('y', 60)
+            .attr('fill', colors[0])
+            .attr('text-anchor', 'end')
+            .attr('dominant-baseline', 'central')
+            .attr('font-size', 25)
+            .style('font-weight', '100')
+            .style('font-family', 'sf-mono')
+
+
+        svg
+            .append('text')
+            .text(nf(100 - stateData[stateData.length - 1].win) + '%')
+            .attr('x', '20')
+            .attr('y', 60)
+            .attr('fill', colors[1])
+            .attr('text-anchor', 'start')
+            .attr('dominant-baseline', 'central')
+            .attr('font-size', 25)
+            .style('font-weight', '100')
+            .style('font-family', 'sf-mono')
+
+        svg
+            .append('text')
+            .text('Joseph Biden')
+            .attr('x', '20')
+            .attr('y', 30)
+            .attr('fill', d => 'black')
+            .attr('text-anchor', 'start')
+            .attr('dominant-baseline', 'central')
+            .attr('font-size', 20)
+            .style('font-weight', '100')
+            .style('font-family', 'sf-mono')
+
+        var dayWidth = 900 / 247
+        var x = d3.scaleTime()
+            .rangeRound([50, 950])
+            .domain([new Date(2020, 2, 1), new Date(2020, 10, 3)])
+
+        svg.append("g")
+            .attr("class", "x-axis")
+            .attr("transform", "translate(0,160)")
+            .call(d3.axisBottom(x).tickSize(-520).ticks(5)
+                .tickFormat(d3.timeFormat("%b")))
+            .call(g => {
+                var years = x.ticks(d3.timeYear.every(1))
+                var xshift = 0
+                g.selectAll("text")
+                    .style("text-anchor", "right")
+                    .attr("y", 15)
+                    .attr('fill', 'black')
+                    .attr('font-size', 15)
+                    .style('font-weight', 100)
+                g.selectAll("line")
+                    .attr("opacity", 0)
+                    .attr("stroke", "grey")
+
+
+                g.select(".domain")
+                    .attr("opacity", 0)
+
+
+            })
+
+        svg.selectAll('sda')
+            .data(stateData)
+            .enter()
+            .append("rect")
+            .attr('width', dayWidth)
+            .attr('height', 50)
+            .attr('y', 100)
+            .attr('x', (d, i) => 50 + i * dayWidth)
+            .attr("fill", d => color(d.win))
+            .attr("stroke", d => color(d.win))
+    }
+
+    var senatemap = d3.select("#senatemap")
+        .append("svg")
+        .attr("viewBox", '0 0 1000 400')
+
+
+    var projection = d3.geoAlbersUsa()
+        .scale([800])
+        .translate([1000 / 2, 400 / 2]);
+
+    var path = d3.geoPath()
+        .projection(projection);
+
+    json.features.forEach((d, i) => {
+        var state = d.properties.name
+        d.properties.abbrev = presInfo.filter(d => d.state == state).length == 0 ? "" : presInfo.filter(d => d.state == state)[0].label
+    })
+
+
+
+    senatemap
+        .selectAll("labels")
+        .data(json.features)
+        .enter()
+        .append("text")
+        .attr("x", function (d) { return path.centroid(d)[0] })
+        .attr("y", function (d) { return path.centroid(d)[1] })
+        .text(function (d) { return d.properties.abbrev })
+        .attr("text-anchor", "middle")
+        .attr("alignment-baseline", "central")
+        .style("font-size", 11)
+        .style("fill", "black")
+
+
+
+}
