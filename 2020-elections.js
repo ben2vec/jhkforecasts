eca8034b258dd6c4beed95d90fe75947737e486d
jhkersting <56937882+jhkersting@users.noreply.github.com>
2020-07-07 22:33:31 -0500

    s

--- 2020-elections.js
+++ 2020-elections.js
@@ -1,6 +1,8 @@
+
+
 var presInfo = [{ 'state': 'Alabama', 'label': 'AL', 'radius': 16.43, 'x': 413, 'y': 332 }, { 'state': 'Alaska', 'label': 'AK', 'radius': 9.49, 'x': 41, 'y': 19 }, { 'state': 'Arizona', 'label': 'AZ', 'radius': 18.17, 'x': 172, 'y': 282 }, { 'state': 'Arkansas', 'label': 'AR', 'radius': 13.42, 'x': 325, 'y': 290 }, { 'state': 'California', 'label': 'CA', 'radius': 40.62, 'x': 103, 'y': 237 }, { 'state': 'Colorado', 'label': 'CO', 'radius': 16.43, 'x': 224, 'y': 249 }, { 'state': 'Connecticut', 'label': 'CT', 'radius': 14.49, 'x': 586, 'y': 128 }, { 'state': 'Delaware', 'label': 'DE', 'radius': 9.49, 'x': 557, 'y': 183 }, { 'state': 'District of Columbia', 'label': 'DC', 'radius': 9.49, 'x': 536, 'y': 193 }, { 'state': 'Florida', 'label': 'FL', 'radius': 29.5, 'x': 483, 'y': 380 }, { 'state': 'Georgia', 'label': 'GA', 'radius': 21.91, 'x': 443, 'y': 298 }, { 'state': 'Hawaii', 'label': 'HI', 'radius': 10.95, 'x': 88, 'y': 372 }, { 'state': 'Idaho', 'label': 'ID', 'radius': 10.95, 'x': 188, 'y': 173 }, { 'state': 'Illinois', 'label': 'IL', 'radius': 24.49, 'x': 359, 'y': 207 }, { 'state': 'Indiana', 'label': 'IN', 'radius': 18.17, 'x': 413, 'y': 207 }, { 'state': 'Iowa', 'label': 'IA', 'radius': 13.42, 'x': 306, 'y': 195 }, { 'state': 'Kansas', 'label': 'KS', 'radius': 13.42, 'x': 266, 'y': 248 }, { 'state': 'Kentucky', 'label': 'KY', 'radius': 15.49, 'x': 411, 'y': 251 }, { 'state': 'Louisiana', 'label': 'LA', 'radius': 15.49, 'x': 326, 'y': 335 }, { 'state': 'Maine', 'label': 'ME', 'radius': 7.75, 'x': 628, 'y': 26 }, { 'state': 'Maryland', 'label': 'MD', 'radius': 17.32, 'x': 505, 'y': 185 }, { 'state': 'Massachusetts', 'label': 'MA', 'radius': 18.17, 'x': 607, 'y': 89 }, { 'state': 'Michigan', 'label': 'MI', 'radius': 21.91, 'x': 418, 'y': 149 }, { 'state': 'Minnesota', 'label': 'MN', 'radius': 17.32, 'x': 304, 'y': 142 }, { 'state': 'Mississippi', 'label': 'MS', 'radius': 13.42, 'x': 373, 'y': 324 }, { 'state': 'Missouri', 'label': 'MO', 'radius': 17.32, 'x': 329, 'y': 251 }, { 'state': 'Montana', 'label': 'MT', 'radius': 9.49, 'x': 206, 'y': 131 }, { 'state': 'Nebraska', 'label': 'NE', 'radius': 7.75, 'x': 258, 'y': 209 }, { 'state': 'Nevada', 'label': 'NV', 'radius': 13.42, 'x': 167, 'y': 220 }, { 'state': 'New Hampshire', 'label': 'NH', 'radius': 10.95, 'x': 612, 'y': 54 }, { 'state': 'New Jersey', 'label': 'NJ', 'radius': 20.49, 'x': 551, 'y': 147 }, { 'state': 'New Mexico', 'label': 'NM', 'radius': 12.25, 'x': 215, 'y': 303 }, { 'state': 'New York', 'label': 'NY', 'radius': 29.5, 'x': 548, 'y': 81 }, { 'state': 'North Carolina', 'label': 'NC', 'radius': 21.21, 'x': 499, 'y': 278 }, { 'state': 'North Dakota', 'label': 'ND', 'radius': 9.49, 'x': 257, 'y': 136 }, { 'state': 'Ohio', 'label': 'OH', 'radius': 23.24, 'x': 459, 'y': 191 }, { 'state': 'Oklahoma', 'label': 'OK', 'radius': 14.49, 'x': 270, 'y': 294 }, { 'state': 'Oregon', 'label': 'OR', 'radius': 14.49, 'x': 124, 'y': 176 }, { 'state': 'Pennsylvania', 'label': 'PA', 'radius': 24.49, 'x': 498, 'y': 132 }, { 'state': 'Rhode Island', 'label': 'RI', 'radius': 10.95, 'x': 619, 'y': 126 }, { 'state': 'South Carolina', 'label': 'SC', 'radius': 16.43, 'x': 487, 'y': 322 }, { 'state': 'South Dakota', 'label': 'SD', 'radius': 9.49, 'x': 257, 'y': 167 }, { 'state': 'Tennessee', 'label': 'TN', 'radius': 18.17, 'x': 379, 'y': 284 }, { 'state': 'Texas', 'label': 'TX', 'radius': 33.76, 'x': 271, 'y': 355 }, { 'state': 'Utah', 'label': 'UT', 'radius': 13.42, 'x': 204, 'y': 218 }, { 'state': 'Vermont', 'label': 'VT', 'radius': 9.49, 'x': 585, 'y': 47 }, { 'state': 'Virginia', 'label': 'VA', 'radius': 19.75, 'x': 508, 'y': 229 }, { 'state': 'Washington', 'label': 'WA', 'radius': 18.97, 'x': 154, 'y': 131 }, { 'state': 'West Virginia', 'label': 'WV', 'radius': 12.25, 'x': 451, 'y': 242 }, { 'state': 'Wisconsin', 'label': 'WI', 'radius': 17.32, 'x': 359, 'y': 146 }, { 'state': 'Wyoming', 'label': 'WY', 'radius': 9.49, 'x': 214, 'y': 177 }, { 'state': 'Maine-1', 'label': '1', 'radius': 5.48, 'x': 612, 'y': 26 }, { 'state': 'Maine-2', 'label': '2', 'radius': 5.48, 'x': 644, 'y': 26 }, { 'state': 'Nebraska-1', 'label': '1', 'radius': 5.48, 'x': 242, 'y': 209 }, { 'state': 'Nebraska-2', 'label': '2', 'radius': 5.48, 'x': 258, 'y': 193 }, { 'state': 'Nebraska-3', 'label': '3', 'radius': 5.48, 'x': 274, 'y': 209 }]
 var colors = ['#FF6060', '#0091FF', '#FFE130']
-var senateStates = [{"state":"Alabama"},{"state":"Alaska"},{"state":"Arizona"},{"state":"Arkansas"},{"state":"Colorado"},{"state":"Delaware"},{"state":"Georgia"},{"state":"Idaho"},{"state":"Illinois"},{"state":"Iowa"},{"state":"Kansas"},{"state":"Kentucky"},{"state":"Louisiana"},{"state":"Maine"},{"state":"Massachusetts"},{"state":"Michigan"},{"state":"Minnesota"},{"state":"Mississippi"},{"state":"Montana"},{"state":"Nebraska"},{"state":"New Hampshire"},{"state":"New Jersey"},{"state":"New Mexico"},{"state":"North Carolina"},{"state":"Oklahoma"},{"state":"Oregon"},{"state":"Rhode Island"},{"state":"South Carolina"},{"state":"South Dakota"},{"state":"Tennessee"},{"state":"Texas"},{"state":"Virginia"},{"state":"West Virginia"},{"state":"Wyoming"},{"state":"Georgia*"}]
+var senateStates = [{ "state": "Alabama", "class": "Class II", "state_index": "Alabama: Class II", "pvi": 30.12, "incumbent": "Doug Jones", "inc_party": "DEM" }, { "state": "Alaska", "class": "Class II", "state_index": "Alaska: Class II", "pvi": 17.09, "incumbent": "Dan Sullivan", "inc_party": "REP" }, { "state": "Arizona", "class": "Class III", "state_index": "Arizona: Class III", "pvi": 7.45, "incumbent": "Martha McSally", "inc_party": "REP" }, { "state": "Arkansas", "class": "Class II", "state_index": "Arkansas: Class II", "pvi": 31.71, "incumbent": "Tom Cotton", "inc_party": "REP" }, { "state": "Colorado", "class": "Class II", "state_index": "Colorado: Class II", "pvi": -2.97, "incumbent": "Cory Gardner", "inc_party": "REP" }, { "state": "Delaware", "class": "Class II", "state_index": "Delaware: Class II", "pvi": -14.75, "incumbent": "Chris Coons", "inc_party": "DEM" }, { "state": "Georgia", "class": "Class II", "state_index": "Georgia: Class II", "pvi": 8.65, "incumbent": "David Perdue", "inc_party": "REP" }, { "state": "Idaho", "class": "Class II", "state_index": "Idaho: Class II", "pvi": 36.88, "incumbent": "Jim Risch", "inc_party": "REP" }, { "state": "Illinois", "class": "Class II", "state_index": "Illinois: Class II", "pvi": -15.12, "incumbent": "Dick Durbin", "inc_party": "DEM" }, { "state": "Iowa", "class": "Class II", "state_index": "Iowa: Class II", "pvi": 6.99, "incumbent": "Joni K. Ernst", "inc_party": "REP" }, { "state": "Kansas", "class": "Class II", "state_index": "Kansas: Class II", "pvi": 21.94, "incumbent": "OPEN", "inc_party": "REP" }, { "state": "Kentucky", "class": "Class II", "state_index": "Kentucky: Class II", "pvi": 30.52, "incumbent": "Mitch McConnell", "inc_party": "REP" }, { "state": "Louisiana", "class": "Class II", "state_index": "Louisiana: Class II", "pvi": 24.86, "incumbent": "Bill Cassidy", "inc_party": "REP" }, { "state": "Maine", "class": "Class II", "state_index": "Maine: Class II", "pvi": -5, "incumbent": "Susan Collins", "inc_party": "REP" }, { "state": "Massachusetts", "class": "Class II", "state_index": "Massachusetts: Class II", "pvi": -26.97, "incumbent": "Ed Markey", "inc_party": "DEM" }, { "state": "Michigan", "class": "Class II", "state_index": "Michigan: Class II", "pvi": 0.42, "incumbent": "Gary Peters", "inc_party": "DEM" }, { "state": "Minnesota", "class": "Class II", "state_index": "Minnesota: Class II", "pvi": -1.82, "incumbent": "Tina Smith", "inc_party": "DEM" }, { "state": "Mississippi", "class": "Class II", "state_index": "Mississippi: Class II", "pvi": 20.6, "incumbent": "Cindy Hyde-Smith", "inc_party": "REP" }, { "state": "Montana", "class": "Class II", "state_index": "Montana: Class II", "pvi": 19.04, "incumbent": "Steve Daines", "inc_party": "REP" }, { "state": "Nebraska", "class": "Class II", "state_index": "Nebraska: Class II", "pvi": 29.51, "incumbent": "Ben Sasse", "inc_party": "REP" }, { "state": "New Hampshire", "class": "Class II", "state_index": "New Hampshire: Class II", "pvi": -0.56, "incumbent": "Jeanne Shaheen", "inc_party": "DEM" }, { "state": "New Jersey", "class": "Class II", "state_index": "New Jersey: Class II", "pvi": -13.2, "incumbent": "Cory Booker", "inc_party": "DEM" }, { "state": "New Mexico", "class": "Class II", "state_index": "New Mexico: Class II", "pvi": -9.07, "incumbent": "OPEN", "inc_party": "DEM" }, { "state": "North Carolina", "class": "Class II", "state_index": "North Carolina: Class II", "pvi": 5.61, "incumbent": "Thom Tillis", "inc_party": "REP" }, { "state": "Oklahoma", "class": "Class II", "state_index": "Oklahoma: Class II", "pvi": 37.51, "incumbent": "Jim Inhofe", "inc_party": "REP" }, { "state": "Oregon", "class": "Class II", "state_index": "Oregon: Class II", "pvi": -10.63, "incumbent": "Jeff Merkey", "inc_party": "DEM" }, { "state": "Rhode Island", "class": "Class II", "state_index": "Rhode Island: Class II", "pvi": -18.88, "incumbent": "Jack Reed", "inc_party": "DEM" }, { "state": "South Carolina", "class": "Class II", "state_index": "South Carolina: Class II", "pvi": 16.72, "incumbent": "Lindesy Graham", "inc_party": "REP" }, { "state": "South Dakota", "class": "Class II", "state_index": "South Dakota: Class II", "pvi": 31.45, "incumbent": "Mike Rounds", "inc_party": "REP" }, { "state": "Tennessee", "class": "Class II", "state_index": "Tennessee: Class II", "pvi": 27.95, "incumbent": "OPEN", "inc_party": "REP" }, { "state": "Texas", "class": "Class II", "state_index": "Texas: Class II", "pvi": 13.49, "incumbent": "John Cornyn", "inc_party": "REP" }, { "state": "Virginia", "class": "Class II", "state_index": "Virginia: Class II", "pvi": -2.88, "incumbent": "Mark Warner", "inc_party": "DEM" }, { "state": "West Virginia", "class": "Class II", "state_index": "West Virginia: Class II", "pvi": 36.68, "incumbent": "Shelley Moore Capito", "inc_party": "REP" }, { "state": "Wyoming", "class": "Class II", "state_index": "Wyoming: Class II", "pvi": 48.09, "incumbent": "OPEN", "inc_party": "REP" }, { "state": "Georgia", "class": "Class III", "state_index": "Georgia: Class III", "pvi": 8.65, "incumbent": "OPEN", "inc_party": "REP" }]
 var category = ['gop', 'dem', 'third']
 
 var cand_colors = d3.scaleOrdinal()
@@ -28,7 +30,13 @@ function ready(error, pres, senate, house, json) {
     var pres = pres.filter(d => d.party == 'gop')
     var prestoday = pres.slice(pres.length - 57, pres.length)
     var trumpWin = +pres[pres.length - 1].win
-
+    var senateToday = senate.slice(senate.length - 106, senate.length)
+    var array = senate.map(d => {
+        return d.forecast_date
+    })
+    const uniqueSet = new Set(array)
+    const senateDates = [...uniqueSet]
+    console.log(senateDates)
     presInfo.forEach((d, i) => {
         var state = d.state
         d.win = +prestoday.filter(d => d.state == state)[0].win
@@ -131,6 +139,7 @@ function ready(error, pres, senate, house, json) {
         var stateData = pres.filter(d => d.state == state)
         var svg = d3.select('#statespres')
             .append('svg')
+            .style("padding-top", "20px")
             .attr('class', 'presChange')
             .attr('viewBox', '0 0 1000 200')
         console.log(stateData)
@@ -251,24 +260,208 @@ function ready(error, pres, senate, house, json) {
 
     json.features.forEach((d, i) => {
         var state = d.properties.name
+        var stateIndex = d.properties.name == "Arizona" ? "Arizona: Class III" : d.properties.name + ": Class II"
         d.properties.abbrev = presInfo.filter(d => d.state == state).length == 0 ? "" : presInfo.filter(d => d.state == state)[0].label
+        var stateData = senateToday.filter(d => d.state_index == stateIndex)
+        d.properties.race = stateData.length == 0 ? "no" : "yes"
+        d.properties.win = stateData.length == 0 ? 0 : d3.sum(stateData.filter(d => d.party == "REP"), d => d.win)
+        d.properties.stateIndex = stateIndex
     })
 
 
+    senatemap.selectAll("hhh")
+        .data(json.features)
+        .enter()
+        .append("path")
+        .attr("class", "states")
+        .attr("d", path)
+        .style("stroke", "#f0f0f0")
+        .style("stroke-width", "1")
+        .style("fill", "none")
+        .style("opacity", 1)
 
     senatemap
         .selectAll("labels")
         .data(json.features)
         .enter()
+        .append("rect")
+        .attr("x", d => path.centroid(d)[0] - 8)
+        .attr("y", d => d.properties.abbrev == "RI" ? path.centroid(d)[1] + 2 : path.centroid(d)[1] - 8)
+        .attr("height", 16)
+        .attr("width", 16)
+        .attr("ry", 2)
+        .style("fill", d => d.properties.race == "no" ? "none" : color(d.properties.win))
+
+    senatemap
+        .append("a")
+        .attr("href", "#statessenate")
+        .append("rect")
+        .attr("x", d => 700)
+        .attr("y", d => 290)
+        .attr("height", 16)
+        .attr("width", 16)
+        .attr("ry", 2)
+        .style("fill", color(d3.sum(senateToday.filter(d => d.state_index == "Georgia: Class III" && d.party == "REP"), d => d.win)))
+        .on("click", d => {
+            senateState("Georgia: Class III")
+        })
+
+    senatemap
         .append("text")
-        .attr("x", function (d) { return path.centroid(d)[0] })
-        .attr("y", function (d) { return path.centroid(d)[1] })
+        .attr("x", d => 708)
+        .attr("y", d => 280)
+        .text("GA*")
+        .attr("text-anchor", "middle")
+        .attr("alignment-baseline", "central")
+        .style("font-size", 10)
+        .style("fill", "black")
+    senatemap
+        .selectAll("labels")
+        .data(json.features)
+        .enter()
+        .append("text")
+        .attr("x", d => path.centroid(d)[0])
+        .attr("y", d => d.properties.abbrev == "RI" ? path.centroid(d)[1] + 10 : path.centroid(d)[1])
         .text(function (d) { return d.properties.abbrev })
         .attr("text-anchor", "middle")
         .attr("alignment-baseline", "central")
-        .style("font-size", 11)
-        .style("fill", "black")
+        .style("font-size", 10)
+        .style("fill", d => d.properties.race == "no" ? "none" : Math.abs(50 - d.properties.win) > 15 ? "white" : "black")
 
+    senatemap
+        .selectAll("labels")
+        .data(json.features)
+        .enter()
+        .append("a")
+        .attr("href", "#statessenate")
+        .append("rect")
+        .attr("class", "statesover")
+        .attr("x", d => path.centroid(d)[0] - 8)
+        .attr("y", d => d.properties.abbrev == "RI" ? path.centroid(d)[1] + 2 : path.centroid(d)[1] - 8)
+        .attr("height", 16)
+        .attr("width", 16)
+        .attr("ry", 2)
+        .on("click", d => {
+            senateState(d.properties.stateIndex)
+        })
+
+    function senateState(stateIndex) {
+
+        d3.select('.senateChange').remove()
+        var stateData = senate.filter(d => d.state_index == stateIndex)
+        var svg = d3.select('#statessenate')
+            .append('svg')
+            .style("padding-top", "20px")
+            .attr('class', 'senateChange')
+            .attr('viewBox', '0 0 1000 200')
+        console.log(stateData)
+        var timeData = []
+        senateDates.forEach((d, i) => {
+            var date = d
+            var win = d3.sum(stateData.filter(d => d.party == "REP" && d.forecast_date == date), d => d.win)
+            timeData.push(win)
+        })
+        svg.append('text')
+            .text(stateData[0].state_index == "Georgia: Class III" ? "Georgia Special" : stateData[0].state)
+            .attr('x', '500')
+            .attr('y', 30)
+            .attr('fill', d => 'black')
+            .attr('text-anchor', 'middle')
+            .attr('dominant-baseline', 'central')
+            .attr('font-size', 30)
+            .style('font-weight', '100')
+            .style('font-family', 'sf-mono')
+
+        svg
+            .append('text')
+            .text('Republicans')
+            .attr('x', '980')
+            .attr('y', 30)
+            .attr('fill', d => 'black')
+            .attr('text-anchor', 'end')
+            .attr('dominant-baseline', 'central')
+            .attr('font-size', 20)
+            .style('font-weight', '100')
+            .style('font-family', 'sf-mono')
+
+        svg
+            .append('text')
+            .text(nf(timeData[timeData.length - 1]) + '%')
+            .attr('x', '980')
+            .attr('y', 60)
+            .attr('fill', colors[0])
+            .attr('text-anchor', 'end')
+            .attr('dominant-baseline', 'central')
+            .attr('font-size', 25)
+            .style('font-weight', '100')
+            .style('font-family', 'sf-mono')
+
+
+        svg
+            .append('text')
+            .text(nf(100 - timeData[timeData.length - 1]) + '%')
+            .attr('x', '20')
+            .attr('y', 60)
+            .attr('fill', colors[1])
+            .attr('text-anchor', 'start')
+            .attr('dominant-baseline', 'central')
+            .attr('font-size', 25)
+            .style('font-weight', '100')
+            .style('font-family', 'sf-mono')
+
+        svg
+            .append('text')
+            .text('Democrats')
+            .attr('x', '20')
+            .attr('y', 30)
+            .attr('fill', d => 'black')
+            .attr('text-anchor', 'start')
+            .attr('dominant-baseline', 'central')
+            .attr('font-size', 20)
+            .style('font-weight', '100')
+            .style('font-family', 'sf-mono')
+
+        var dayWidth = 900 / 216
+        var x = d3.scaleTime()
+            .rangeRound([50, 950])
+            .domain([new Date(2020, 3, 1), new Date(2020, 10, 3)])
+
+        svg.append("g")
+            .attr("class", "x-axis")
+            .attr("transform", "translate(0,160)")
+            .call(d3.axisBottom(x).tickSize(-520).ticks(5)
+                .tickFormat(d3.timeFormat("%b")))
+            .call(g => {
+                var years = x.ticks(d3.timeYear.every(1))
+                var xshift = 0
+                g.selectAll("text")
+                    .style("text-anchor", "right")
+                    .attr("y", 15)
+                    .attr('fill', 'black')
+                    .attr('font-size', 15)
+                    .style('font-weight', 100)
+                g.selectAll("line")
+                    .attr("opacity", 0)
+                    .attr("stroke", "grey")
+
+
+                g.select(".domain")
+                    .attr("opacity", 0)
+
+
+            })
+
+        svg.selectAll('sda')
+            .data(timeData)
+            .enter()
+            .append("rect")
+            .attr('width', dayWidth)
+            .attr('height', 50)
+            .attr('y', 100)
+            .attr('x', (d, i) => 50 + i * dayWidth)
+            .attr("fill", d => color(d))
+            .attr("stroke", d => color(d))
+    }
 
 
 }
