0fa05be62ef739d88c2d67b60aed83d667a98f33
jhkersting <56937882+jhkersting@users.noreply.github.com>
2020-08-17 15:12:26 -0500

    s

--- presidential-forecast/nationalphone.js
+++ presidential-forecast/nationalphone.js
@@ -550,8 +550,8 @@ d3.csv("https://data.jhkforecasts.com/2020-presidential.csv", function (data) {
         d.conf = candsData.map(((d, j) => {
           return {
             date: d.forecastDate,
-            top: input == "win" ? d[input] : input == "ev" ? +d[input] + +d.p10 * 1.3 : i < 2 ? +d[input] + (4.75 - (j / 150)) : +d[input] + (+d[input] + 3) / 2,
-            bottom: input == "win" ? d[input] : input == "ev" ? +d[input] - +d.p10 * 1.3 : i < 2 ? +d[input] - (4.75 - (j / 150)) : +d[input] - (+d[input]) / 1.5,
+            top: input == "win" ? d[input] : input == "ev" ? +d[input] + +d.p10 * 1.3 : i < 2 ? +d[input] + (5.25 - (j / 150)) : +d[input] + (+d[input] + 3) / 2,
+            bottom: input == "win" ? d[input] : input == "ev" ? +d[input] - +d.p10 * 1.3 : i < 2 ? +d[input] - (5.25 - (j / 150)) : +d[input] - (+d[input]) / 1.5,
           }
         }))
       })
